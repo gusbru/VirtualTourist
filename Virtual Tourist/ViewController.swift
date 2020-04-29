e4e8c3430523ff6b29a083e8eb92bb12e4159985
Gustavo Brunetto <gustavobruneto@gmail.com>
2020-04-29 13:14:04 -0700

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Gustavo Brunetto on 2020-04-29.
+//  Copyright © 2020 Gustavo Brunetto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
